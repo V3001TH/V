47fb0047c00db7d1637b2b7dd13ba94d3c19e5d5
V3001TH <108760697+V3001TH@users.noreply.github.com>
2022-07-15 16:23:32 +0200

    Add files via upload

---
+++ labolatorna 1-1.py
@@ -0,0 +1,11 @@
+import  математики
+
+x  =  float ( input ( 'Введіть x: ' ))
+y  =  float ( input ( 'Введіть y: ' ))
+z  =  float ( input ( 'Введіть z: ' ))
+
+a  =(((**|x-1|)-(**|y|))/(1+(x^2/2)+(y^3/4)))
+b  = (x(arctg(z)+6))
+
+print ( 'Рівняння a= ' , a )
+print ( 'Рівняння b= ' , b )
