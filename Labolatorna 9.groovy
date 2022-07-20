6813abf9a5ab201303bb1aacc99b4b2919207319
V3001TH <108760697+V3001TH@users.noreply.github.com>
2022-07-20 17:23:46 +0200

    Update Labolatorna 9.groovy

--- Labolatorna 9.groovy
+++ Labolatorna 9.groovy
@@ -1,6 +1,6 @@
 begin
   Write('"Іриски"   '); Write('1 кг   '); Writeln('68 грн');
-  Write('"Млинці"  ');  Write('250 гр '); Writeln('26 грн');
+  Write('"Млинці"  '); Write('250 гр '); Writeln('26 грн');
   Write('"Фруктовий салат"  '); Write('125 гр'); Writen('45 грн')
   Write('"Сок"  '); Write('150 мл'); Writen('12 грн')
   
